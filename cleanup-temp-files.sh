e048692613cd61cdb2ccac1e0a87f781ae07680c
Ben Blum <bblum@andrew.cmu.edu>
2016-06-14 22:58:34 -0400

    add scripts

---
+++ cleanup-temp-files.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -d "ls" -o ! -d "qs" ]; then
+	echo "cleanup script run from wrong directory; i don't see ls/ or qs/"
+	exit 1
+fi
+
+rm -f qs/ls-output.log.*
+rm -f qs/ls-setup.log.*
+rm -f ls/config.quicksand.*
+rm -f ls/pps-and-such.quicksand.*
